0681b771bcf2cf86869e2608b09a85deef1b5955
Siddharth Kumar Jha <siddharth.k.jha@proton.me>
2025-06-16 18:03:23 +0530

    AOC 24 Q6 P2 -- intermediate soln

---
+++ Q6/TraceSoldierPath.zig
@@ -0,0 +1,171 @@
+const std = @import("std");
+const splitByLine = @import("./split-by-line.zig");
+
+pub const string = splitByLine.string;
+pub const lines = splitByLine.lines;
+pub const Allocator = std.mem.Allocator;
+
+pub const Direction = enum { Up, Down, Left, Right };
+pub const GuardPos = struct {
+    line: usize,
+    col: usize,
+};
+pub const MoveResult = enum { OutOfBounds, ObstacleCollision };
+pub const MoveConst = struct {
+    pub const Visited: u8 = 'X';
+    pub const Obstacle: u8 = '#';
+    pub const Guard: u8 = '^';
+};
+
+const allocator = std.heap.page_allocator;
+const cwd = std.fs.cwd();
+
+pub fn findPosOfGuard(array_of_lines: *lines) ?GuardPos {
+    for (array_of_lines.items, 0..) |line, line_idx| {
+        if (std.mem.indexOfScalar(u8, line, MoveConst.Guard)) |index_of_guard| {
+            return GuardPos{
+                .line = line_idx,
+                .col = index_of_guard,
+            };
+        }
+    }
+    return null;
+}
+
+fn MoveRight(array_of_lines: *lines, pos: *GuardPos) MoveResult {
+    const line_idx = pos.line;
+    const col_idx = &pos.col;
+    const max_col_limit = array_of_lines.items[0].len;
+
+    while (col_idx.* < max_col_limit) {
+        const next_col = col_idx.* + 1;
+        if (next_col >= max_col_limit) return MoveResult.OutOfBounds;
+        if (array_of_lines.items[line_idx][next_col] != MoveConst.Obstacle) {
+            array_of_lines.items[line_idx][col_idx.*] = MoveConst.Visited;
+            col_idx.* = next_col;
+            array_of_lines.items[line_idx][col_idx.*] = MoveConst.Guard;
+        } else {
+            return MoveResult.ObstacleCollision;
+        }
+    } else {
+        return MoveResult.OutOfBounds;
+    }
+}
+
+fn MoveLeft(array_of_lines: *lines, pos: *GuardPos) MoveResult {
+    const line_idx = pos.line;
+    var col_idx: i64 = @intCast(pos.col);
+    const max_col_limit = array_of_lines.items[0].len;
+
+    while (col_idx < max_col_limit and col_idx >= 0) {
+        const next_col = col_idx - 1;
+        if (next_col >= max_col_limit or next_col < 0) return MoveResult.OutOfBounds;
+        if (array_of_lines.items[line_idx][@intCast(next_col)] != MoveConst.Obstacle) {
+            array_of_lines.items[line_idx][@intCast(col_idx)] = MoveConst.Visited;
+            col_idx = next_col;
+            pos.col = @intCast(col_idx);
+            array_of_lines.items[line_idx][@intCast(col_idx)] = MoveConst.Guard;
+        } else {
+            return MoveResult.ObstacleCollision;
+        }
+    } else {
+        return MoveResult.OutOfBounds;
+    }
+}
+
+fn MoveDown(array_of_lines: *lines, pos: *GuardPos) MoveResult {
+    const line_idx = &pos.line;
+    const col_idx = pos.col;
+    const max_line_limit = array_of_lines.items.len;
+
+    while (line_idx.* < max_line_limit) {
+        const next_line = line_idx.* + 1;
+        if (next_line >= max_line_limit) return MoveResult.OutOfBounds;
+        if (array_of_lines.items[next_line][col_idx] != MoveConst.Obstacle) {
+            array_of_lines.items[line_idx.*][col_idx] = MoveConst.Visited;
+            line_idx.* = next_line;
+            array_of_lines.items[next_line][col_idx] = MoveConst.Guard;
+        } else {
+            return MoveResult.ObstacleCollision;
+        }
+    } else {
+        return MoveResult.OutOfBounds;
+    }
+}
+
+fn MoveUp(array_of_lines: *lines, pos: *GuardPos) MoveResult {
+    var line_idx: i64 = @intCast(pos.line);
+    const col_idx = pos.col;
+    const max_line_limit = array_of_lines.items.len;
+
+    while (line_idx < max_line_limit and line_idx >= 0) {
+        const next_line = line_idx - 1;
+        if (next_line >= max_line_limit or next_line < 0) return MoveResult.OutOfBounds;
+        if (array_of_lines.items[@intCast(next_line)][col_idx] != MoveConst.Obstacle) {
+            array_of_lines.items[@intCast(line_idx)][col_idx] = MoveConst.Visited;
+            line_idx = @intCast(next_line);
+            pos.line = @intCast(line_idx);
+            array_of_lines.items[@intCast(next_line)][col_idx] = MoveConst.Guard;
+        } else {
+            return MoveResult.ObstacleCollision;
+        }
+    } else {
+        return MoveResult.OutOfBounds;
+    }
+}
+
+fn MoveSoldier(array_of_lines: *lines, pos: *GuardPos, direction: Direction) MoveResult {
+    const line_idx = pos.line;
+    const col_idx = pos.col;
+
+    const max_line_limit = array_of_lines.items.len;
+    if (max_line_limit <= 0 or line_idx >= max_line_limit) return MoveResult.OutOfBounds;
+    const max_col_limit = array_of_lines.items[0].len;
+    if (max_col_limit <= 0 or col_idx >= max_col_limit) return MoveResult.OutOfBounds;
+
+    switch (direction) {
+        .Up => return MoveUp(array_of_lines, pos),
+        .Down => return MoveDown(array_of_lines, pos),
+        .Left => return MoveLeft(array_of_lines, pos),
+        .Right => return MoveRight(array_of_lines, pos),
+    }
+}
+
+pub fn RotateRight(direction: Direction) Direction {
+    switch (direction) {
+        .Up => return .Right,
+        .Down => return .Left,
+        .Left => return .Up,
+        .Right => return .Down,
+    }
+}
+
+fn DistinctPosVisited(buffer: *const string) u64 {
+    const pattern_to_find = [_]u8{MoveConst.Visited};
+    return std.mem.count(u8, buffer.items, pattern_to_find[0..]) + 1;
+}
+
+pub fn TraceSoldierPath(buffer: *string, array_of_lines: *lines) !u64 {
+    const fstream = try cwd.openFile("./example.txt", .{});
+    defer fstream.close();
+
+    const metadata = try fstream.metadata();
+    const size_of_file = metadata.size();
+
+    buffer.* = string.fromOwnedSlice(allocator, try fstream.readToEndAlloc(allocator, size_of_file));
+    array_of_lines.* = lines.fromOwnedSlice(allocator, try splitByLine.convertToSliceOfLines(allocator, buffer));
+
+    const inital_pos = findPosOfGuard(array_of_lines).?;
+    var pos = inital_pos;
+    var direction = Direction.Up;
+    while (MoveSoldier(array_of_lines, &pos, direction) != .OutOfBounds) {
+        std.log.debug("Soldier collided at line: {}, col: {}", pos);
+        direction = RotateRight(direction);
+    } else {
+        std.log.debug("Soldier went OOB at line: {}, col: {}", pos);
+        array_of_lines.items[inital_pos.line][inital_pos.col] = MoveConst.Guard;
+        array_of_lines.items[pos.line][pos.col] = MoveConst.Visited;
+    }
+
+    return DistinctPosVisited(buffer);
+}
--- Q6/part2.zig
+++ Q6/part2.zig
@@ -1,49 +1,42 @@
 const std = @import("std");
-const splitByLine = @import("./split-by-line.zig");
+const TraceSoldierPath = @import("./TraceSoldierPath.zig");
 
-pub const string = splitByLine.string;
-pub const lines = splitByLine.lines;
-pub const Allocator = std.mem.Allocator;
+const string = TraceSoldierPath.string;
+const lines = TraceSoldierPath.lines;
 
-const Direction = enum { Up, Down, Left, Right };
-pub const GuardPos = struct {
-    line: usize,
-    col: usize,
+const Direction = TraceSoldierPath.Direction;
+const GuardPos = TraceSoldierPath.GuardPos;
+const MoveResult = TraceSoldierPath.MoveResult;
+const HashSet = std.hash_map.AutoHashMap(AreaPos, void);
+
+const LoopConst = struct {
+    usingnamespace TraceSoldierPath.MoveConst;
+    const Loop: u8 = '0';
 };
-const MoveResult = enum { OutOfBounds, ObstacleCollision };
-const MoveConst = struct {
-    const Visited = 'X';
-    const Obstacle = '#';
-    const Guard = '^';
+const AreaPos = struct {
+    pos: GuardPos,
+    direction: Direction,
 };
 
 const allocator = std.heap.page_allocator;
 const cwd = std.fs.cwd();
 
-fn findPosOfGuard(array_of_lines: *lines) ?GuardPos {
-    for (array_of_lines.items, 0..) |line, line_idx| {
-        if (std.mem.indexOfScalar(u8, line, MoveConst.Guard)) |index_of_guard| {
-            return GuardPos{
-                .line = line_idx,
-                .col = index_of_guard,
-            };
-        }
-    }
-    return null;
-}
-
-fn MoveRight(array_of_lines: *lines, pos: *GuardPos) MoveResult {
+fn MoveRight(array_of_lines: *lines, pos: *GuardPos, set: *HashSet) !MoveResult {
     const line_idx = pos.line;
     const col_idx = &pos.col;
     const max_col_limit = array_of_lines.items[0].len;
+    const direction = .Right;
 
     while (col_idx.* < max_col_limit) {
         const next_col = col_idx.* + 1;
         if (next_col >= max_col_limit) return MoveResult.OutOfBounds;
-        if (array_of_lines.items[line_idx][next_col] != MoveConst.Obstacle) {
-            array_of_lines.items[line_idx][col_idx.*] = MoveConst.Visited;
+        if (array_of_lines.items[line_idx][next_col] != LoopConst.Obstacle) {
+            _ = try set.getOrPut(AreaPos{ .pos = pos.*, .direction = direction });
+            const loop = doesPathLoop(pos, set, direction);
             col_idx.* = next_col;
-            array_of_lines.items[line_idx][col_idx.*] = MoveConst.Guard;
+            if (loop) {
+                array_of_lines.items[line_idx][col_idx.*] = LoopConst.Loop;
+            }
         } else {
             return MoveResult.ObstacleCollision;
         }
@@ -52,19 +45,23 @@ fn MoveRight(array_of_lines: *lines, pos: *GuardPos) MoveResult {
     }
 }
 
-fn MoveLeft(array_of_lines: *lines, pos: *GuardPos) MoveResult {
+fn MoveLeft(array_of_lines: *lines, pos: *GuardPos, set: *HashSet) !MoveResult {
     const line_idx = pos.line;
     var col_idx: i64 = @intCast(pos.col);
     const max_col_limit = array_of_lines.items[0].len;
+    const direction = .Left;
 
     while (col_idx < max_col_limit and col_idx >= 0) {
         const next_col = col_idx - 1;
         if (next_col >= max_col_limit or next_col < 0) return MoveResult.OutOfBounds;
-        if (array_of_lines.items[line_idx][@intCast(next_col)] != MoveConst.Obstacle) {
-            array_of_lines.items[line_idx][@intCast(col_idx)] = MoveConst.Visited;
+        if (array_of_lines.items[line_idx][@intCast(next_col)] != LoopConst.Obstacle) {
+            _ = try set.getOrPut(AreaPos{ .pos = pos.*, .direction = direction });
+            const loop = doesPathLoop(pos, set, direction);
             col_idx = next_col;
             pos.col = @intCast(col_idx);
-            array_of_lines.items[line_idx][@intCast(col_idx)] = MoveConst.Guard;
+            if (loop) {
+                array_of_lines.items[line_idx][@intCast(col_idx)] = LoopConst.Loop;
+            }
         } else {
             return MoveResult.ObstacleCollision;
         }
@@ -73,18 +70,22 @@ fn MoveLeft(array_of_lines: *lines, pos: *GuardPos) MoveResult {
     }
 }
 
-fn MoveDown(array_of_lines: *lines, pos: *GuardPos) MoveResult {
+fn MoveDown(array_of_lines: *lines, pos: *GuardPos, set: *HashSet) !MoveResult {
     const line_idx = &pos.line;
     const col_idx = pos.col;
     const max_line_limit = array_of_lines.items.len;
+    const direction = .Down;
 
     while (line_idx.* < max_line_limit) {
         const next_line = line_idx.* + 1;
         if (next_line >= max_line_limit) return MoveResult.OutOfBounds;
-        if (array_of_lines.items[next_line][col_idx] != MoveConst.Obstacle) {
-            array_of_lines.items[line_idx.*][col_idx] = MoveConst.Visited;
+        if (array_of_lines.items[next_line][col_idx] != LoopConst.Obstacle) {
+            _ = try set.getOrPut(AreaPos{ .pos = pos.*, .direction = direction });
+            const loop = doesPathLoop(pos, set, direction);
             line_idx.* = next_line;
-            array_of_lines.items[next_line][col_idx] = MoveConst.Guard;
+            if (loop) {
+                array_of_lines.items[next_line][col_idx] = LoopConst.Loop;
+            }
         } else {
             return MoveResult.ObstacleCollision;
         }
@@ -93,19 +94,23 @@ fn MoveDown(array_of_lines: *lines, pos: *GuardPos) MoveResult {
     }
 }
 
-fn MoveUp(array_of_lines: *lines, pos: *GuardPos) MoveResult {
+fn MoveUp(array_of_lines: *lines, pos: *GuardPos, set: *HashSet) !MoveResult {
     var line_idx: i64 = @intCast(pos.line);
     const col_idx = pos.col;
     const max_line_limit = array_of_lines.items.len;
+    const direction = .Up;
 
     while (line_idx < max_line_limit and line_idx >= 0) {
         const next_line = line_idx - 1;
         if (next_line >= max_line_limit or next_line < 0) return MoveResult.OutOfBounds;
-        if (array_of_lines.items[@intCast(next_line)][col_idx] != MoveConst.Obstacle) {
-            array_of_lines.items[@intCast(line_idx)][col_idx] = MoveConst.Visited;
+        if (array_of_lines.items[@intCast(next_line)][col_idx] != LoopConst.Obstacle) {
+            _ = try set.getOrPut(AreaPos{ .pos = pos.*, .direction = direction });
+            const loop = doesPathLoop(pos, set, direction);
             line_idx = @intCast(next_line);
             pos.line = @intCast(line_idx);
-            array_of_lines.items[@intCast(next_line)][col_idx] = MoveConst.Guard;
+            if (loop) {
+                array_of_lines.items[@intCast(next_line)][col_idx] = LoopConst.Loop;
+            }
         } else {
             return MoveResult.ObstacleCollision;
         }
@@ -114,7 +119,17 @@ fn MoveUp(array_of_lines: *lines, pos: *GuardPos) MoveResult {
     }
 }
 
-fn MoveSoldier(array_of_lines: *lines, pos: *GuardPos, direction: Direction) MoveResult {
+fn doesPathLoop(pos: *const GuardPos, set: *HashSet, direction: Direction) bool {
+    const new_direction = TraceSoldierPath.RotateRight(direction);
+    const path_visited = set.contains(AreaPos{ .pos = pos.*, .direction = new_direction });
+    if (path_visited) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+fn MoveSoldier(array_of_lines: *lines, pos: *GuardPos, set: *HashSet, direction: Direction) !MoveResult {
     const line_idx = pos.line;
     const col_idx = pos.col;
 
@@ -124,51 +139,47 @@ fn MoveSoldier(array_of_lines: *lines, pos: *GuardPos, direction: Direction) Mov
     if (max_col_limit <= 0 or col_idx >= max_col_limit) return MoveResult.OutOfBounds;
 
     switch (direction) {
-        .Up => return MoveUp(array_of_lines, pos),
-        .Down => return MoveDown(array_of_lines, pos),
-        .Left => return MoveLeft(array_of_lines, pos),
-        .Right => return MoveRight(array_of_lines, pos),
+        .Up => return MoveUp(array_of_lines, pos, set),
+        .Down => return MoveDown(array_of_lines, pos, set),
+        .Left => return MoveLeft(array_of_lines, pos, set),
+        .Right => return MoveRight(array_of_lines, pos, set),
     }
 }
 
-fn RotateRight(direction: Direction) Direction {
-    switch (direction) {
-        .Up => return .Right,
-        .Down => return .Left,
-        .Left => return .Up,
-        .Right => return .Down,
+fn TraceLoopsInPath(buffer: *string, array_of_lines: *lines, set: *HashSet) !usize {
+    const inital_pos = TraceSoldierPath.findPosOfGuard(array_of_lines).?;
+    var pos = inital_pos;
+    var direction = Direction.Up;
+    while (try MoveSoldier(array_of_lines, &pos, set, direction) != .OutOfBounds) {
+        std.log.debug("Soldier collided at line: {}, col: {}", pos);
+        direction = TraceSoldierPath.RotateRight(direction);
+    } else {
+        std.log.debug("Soldier went OOB at line: {}, col: {}", pos);
+        array_of_lines.items[inital_pos.line][inital_pos.col] = LoopConst.Guard;
+        array_of_lines.items[pos.line][pos.col] = LoopConst.Visited;
     }
-}
 
-fn DistinctPosVisited(buffer: *const string) u64 {
-    const pattern_to_find = [_]u8{MoveConst.Visited};
-    return std.mem.count(u8, buffer.items, pattern_to_find[0..]) + 1;
+    const pattern_to_find = [_]u8{LoopConst.Loop};
+    return std.mem.count(u8, buffer.items, pattern_to_find[0..]);
 }
 
 pub fn main() !void {
-    const fstream = try cwd.openFile("./example.txt", .{});
-    defer fstream.close();
-
-    const metadata = try fstream.metadata();
-    const size_of_file = metadata.size();
-
-    var buffer = string.fromOwnedSlice(allocator, try fstream.readToEndAlloc(allocator, size_of_file));
+    var buffer = string.init(allocator);
     defer buffer.deinit();
 
-    var array_of_lines: lines = lines.fromOwnedSlice(allocator, try splitByLine.convertToSliceOfLines(allocator, &buffer));
+    var array_of_lines = lines.init(allocator);
     defer array_of_lines.deinit();
 
-    var pos = findPosOfGuard(&array_of_lines).?;
-    var direction = Direction.Up;
-    while (MoveSoldier(&array_of_lines, &pos, direction) != .OutOfBounds) {
-        std.log.debug("Soldier collided at line: {}, col: {}", pos);
-        direction = RotateRight(direction);
-    } else {
-        std.log.debug("Soldier went OOB at line: {}, col: {}", pos);
-    }
-
-    const distinct_pos_visited = DistinctPosVisited(&buffer);
-    std.log.debug("\n{s}\n", .{buffer.items});
+    const distinct_pos_visited = try TraceSoldierPath.TraceSoldierPath(&buffer, &array_of_lines);
     std.log.debug("======================================================", .{});
     std.log.debug("no of distinct positions visited: {}", .{distinct_pos_visited});
+
+    var visited_before = HashSet.init(allocator);
+    defer visited_before.deinit();
+
+    try visited_before.ensureUnusedCapacity(@intCast(distinct_pos_visited));
+
+    const no_of_loops = try TraceLoopsInPath(&buffer, &array_of_lines, &visited_before);
+    std.log.debug("\n{s}\n", .{buffer.items});
+    std.log.debug("loop count: {}", .{no_of_loops});
 }
